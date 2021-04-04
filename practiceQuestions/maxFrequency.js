e139522bcf542aa4514b3b6d1880c5cc56745a37
Shivang Chaturvedi <shiva.chaturvedi2191@outlook.com>
2021-04-04 20:51:55 +0530

    Solution for 'Max Frequency in an array'

--- practiceQuestions/maxFrequency.js
+++ practiceQuestions/maxFrequency.js
@@ -1,3 +1,20 @@
-import question from '../assets/readline-async-question';
+let question = require('../assets/readline-async-question');
 
-let answer = ''
+module.exports = async () => {
+  let answer = await question('Enter the array: ');
+  arr = answer.split(" ");
+  let newArr = [];
+  if (arr && arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      var el = arr[i]
+      var b = arr.filter((d) => el === d);
+      newArr.push({ el: el, frequency: b.length })
+    }
+
+    newArr = [...new Set(newArr)];
+    newArr = newArr.sort((a, b) =>
+      (a.frequency < b.frequency) ? 1 : (a.frequency > b.frequency) ? -1 : 0
+    )
+    if (newArr.length > 0 && newArr[0].frequency) console.log(`The max frequency is ${newArr[0].frequency}`)
+  }
+}
